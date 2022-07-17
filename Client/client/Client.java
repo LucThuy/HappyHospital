1791a9bb0f4dfb078c2026784b9eaa3665b69274
LucThuy <102579092+LucThuy@users.noreply.github.com>
2022-07-17 10:28:48 +0700

    Add files via upload

---
+++ Client/client/Client.java
@@ -0,0 +1,174 @@
+package client;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.json.simple.parser.ParseException;
+
+import scene.Container;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.Box;
+
+public class Client extends JFrame {
+
+	private JPanel contentPane;
+	private static Client frame;
+	
+	private String clientName;
+	
+	private Container container;
+	
+	private ChillThread chillThread;
+	private RageThread rageThread;
+	
+	private Socket socket;
+
+	static GraphicsDevice device = GraphicsEnvironment
+	        .getLocalGraphicsEnvironment().getScreenDevices()[0];
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame = new Client();
+					frame.setVisible(true);
+					frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+//					frame.setUndecorated(true);
+					frame.setVisible(true);
+//					device.setFullScreenWindow(frame);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 * @throws ParseException 
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
+	 */
+	public Client() throws FileNotFoundException, IOException, ParseException {
+		setTitle("Con cho");
+		setUI();
+		
+		this.clientName = "minh";
+		createChillThread();
+	}
+	
+	private void setUI() throws FileNotFoundException, IOException, ParseException {
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		addWindowListener(new CustomWindowListener());
+		setBounds(0, 0, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		this.container = new Container(this);
+		contentPane.add(container, BorderLayout.CENTER);
+	}
+	
+	public void createChillThread() {
+		chillThread = new ChillThread(this);
+		chillThread.start();
+	}
+	
+	public void createRageThread(int port) {
+		rageThread = new RageThread(this, port);
+		rageThread.start();
+	}
+	
+	class CustomWindowListener implements WindowListener {
+
+		@Override
+		public void windowOpened(WindowEvent e) {
+			
+		}
+
+		@Override
+		public void windowClosing(WindowEvent e) {
+			try {
+				if(rageThread != null) {
+					rageThread.byeBye();
+				}
+				chillThread.byeBye();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			System.exit(0);
+		}
+
+		@Override
+		public void windowClosed(WindowEvent e) {
+			
+		}
+
+		@Override
+		public void windowIconified(WindowEvent e) {
+			
+		}
+
+		@Override
+		public void windowDeiconified(WindowEvent e) {
+			
+		}
+
+		@Override
+		public void windowActivated(WindowEvent e) {
+			
+		}
+
+		@Override
+		public void windowDeactivated(WindowEvent e) {
+			
+		}		
+	}
+	
+	public Container aloContainer() {
+		return container;
+	}
+	
+	public ChillThread getChillThread() {
+		return chillThread;
+	}
+	
+	public RageThread getRageThread() {
+		return rageThread;
+	}
+	
+	public String getClientName() {
+		return this.clientName;
+	}
+	
+	public void setClientName(String name) {
+		this.clientName = name;
+	}
+}
---
+++ Client/client/RageThread.java
@@ -0,0 +1,180 @@
+package client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+import org.json.simple.parser.ParseException;
+
+import scene.MutilPlayerScene;
+
+public class RageThread extends Thread {
+	private Socket socket;
+	private Client client;
+	private int port;
+	
+	private MutilPlayerScene playScene;
+	
+	private boolean run = true;
+	
+	public RageThread(Client client, int port) {
+		this.client = client;
+		this.port = port;
+	}
+	
+	private InputStream inputStream;
+	private Scanner scan;
+	
+	private OutputStream outputStream;
+	private PrintWriter print;
+	
+	public void run() {
+		try {
+			socket = new Socket("127.0.0.1", port);
+			
+			inputStream = socket.getInputStream();
+		    scan = new Scanner(inputStream);
+		    
+		    outputStream = socket.getOutputStream();
+		    print = new PrintWriter(outputStream, true);
+		    
+		    while(run) {	    	
+		    	if(scan.hasNextLine()) {
+		    		String strReceive = scan.nextLine();
+
+//			        System.out.println("Receive from client: " + strReceive);
+			        if(strReceive != null) {
+				        parseCommand(strReceive);
+			        }
+		    	}
+		    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void parseCommand(String command) throws IOException {
+		String[] cmdList = command.split("\\s");
+		if(cmdList[0].equals("getGo")) {
+			getGo();
+		}
+		else if(cmdList[0].equals("getClientName")) {
+			getClientName();
+		}
+		else if(cmdList[0].equals("addPlayerHost")) {
+			PlayerC player = new PlayerC(Integer.parseInt(cmdList[1]), Integer.parseInt(cmdList[2]), cmdList[3]);
+			addPlayerHost(player);
+		}
+		else if(cmdList[0].equals("genEndPointHost")) {
+			genEndPointHost(Integer.parseInt(cmdList[1]));
+		}
+		else if(cmdList[0].equals("addPlayerGuest")) {
+			PlayerC player = new PlayerC(Integer.parseInt(cmdList[1]), Integer.parseInt(cmdList[2]), cmdList[3]);
+			addPlayerGuest(player);
+		}
+		else if(cmdList[0].equals("genEndPointGuest")) {
+			genEndPointGuest(Integer.parseInt(cmdList[1]));
+		}
+		else if(cmdList[0].equals("playerHostMove")) {
+			playerHostMove(Integer.parseInt(cmdList[1]), Integer.parseInt(cmdList[2]));
+		}
+		else if(cmdList[0].equals("playerGuestMove")) {
+			playerGuestMove(Integer.parseInt(cmdList[1]), Integer.parseInt(cmdList[2]));
+		}
+		else if(cmdList[0].equals("addCat")) {
+			addCat(Integer.parseInt(cmdList[1]), Integer.parseInt(cmdList[2]), Integer.parseInt(cmdList[3]), Integer.parseInt(cmdList[4]));
+		}
+		else if(cmdList[0].equals("catMove")) {
+			catMove(Integer.parseInt(cmdList[1]), Integer.parseInt(cmdList[2]), Integer.parseInt(cmdList[3]));
+		}
+		else if(cmdList[0].equals("catDone")) {
+			catDone(Integer.parseInt(cmdList[1]));
+		}
+		else if(cmdList[0].equals("calScoreHost")) {
+			calScoreHost(cmdList[1]);
+		}
+		else if(cmdList[0].equals("calScoreGuest")) {
+			calScoreGuest(cmdList[1]);
+		}
+	}
+	
+	private void getGo() {
+		try {
+			this.client.aloContainer().showMutilPlayerScene();
+			this.playScene = this.client.aloContainer().getMutilPlayerScene();
+			this.playScene.setHost();
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void getClientName() {
+		print.println("clientName " + this.client.getClientName());
+	}
+	
+	private void addPlayerHost(PlayerC player) {
+		playScene.getBoard().setPlayerHost(player);
+	}
+	
+	private void addPlayerGuest(PlayerC player) {
+		playScene.getBoard().setPlayerGuest(player);
+	}
+	
+	private void genEndPointHost(int endDoorHostID) {
+		playScene.getBoard().setEndDoorHostID(endDoorHostID);
+	}
+	
+	private void genEndPointGuest(int endDoorGuestID) {
+		playScene.getBoard().setEndDoorGuestID(endDoorGuestID);
+	}
+	
+	private void playerHostMove(int x, int y) {
+		playScene.getBoard().setPositionPlayerHost(x, y);
+	}
+	
+	private void playerGuestMove(int x, int y) {
+		playScene.getBoard().setPositionPlayerGuest(x, y);
+	}
+	
+	private void addCat(int x, int y, int id, int end) throws IOException {
+		playScene.getBoard().addCat(x, y, id, end);
+	}
+	
+	private void catMove(int x, int y, int i) {
+		playScene.getBoard().catMove(x, y, i);
+	}
+	
+	private void catDone(int i) {
+		playScene.getBoard().catDone(i);
+	}
+	
+	private void calScoreHost(String score) {
+		playScene.getBoard().setScoreHost(score);
+	}
+	
+	private void calScoreGuest(String score) {
+		playScene.getBoard().setScoreGuest(score);
+	}
+	
+	public void keyPressed(String key, int isHost) {
+		print.println("keyPressed " + key + " " + isHost);
+	}
+	
+	public void keyReleased(String key, int isHost) {
+		print.println("keyReleased " + key + " " + isHost);
+	}
+	
+	public void byeBye() throws IOException {
+		print.println("rageByeBye");
+		
+		socket.close();
+		this.run = false;
+	}
+	
+	public void setPlayScene(MutilPlayerScene playScene) {
+		this.playScene = playScene;
+	}
+}
