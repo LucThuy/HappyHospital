4b9b85f4a17439a70e883b4b89f7cad02bddca60
naman <naman@redstonemaster>
2022-07-22 16:20:42 +0700

    winScene

--- Client/scene/scene/WinScene.java
+++ Client/scene/scene/WinScene.java
@@ -1,6 +1,8 @@
 package scene;
 
 import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -15,6 +17,9 @@ import java.awt.GridBagConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
+
+import scene.HowToPlayScene.MouseBack;
+
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import javax.swing.SpringLayout;
@@ -22,37 +27,61 @@ import javax.swing.SpringLayout;
 import org.json.simple.parser.ParseException;
 
 import java.awt.Font;
+import java.awt.Graphics;
 
 public class WinScene extends JPanel {
 
 	public Container container;
+	private JButton btnHome;
 	
 	/**
 	 * Create the panel.
 	 */
 	public WinScene(Container container) {
 		this.container = container;
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{5.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{5.0, 1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
 		setUp();
 	}
 	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		ImageIcon background = new ImageIcon("data/winScene.png");
+		g.drawImage(background.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+	}
+	
 	public void setUp() {
-		setBackground(Color.PINK);
-		setLayout(new BorderLayout(0, 0));
-		
-		JButton replay = new JButton("REPLAY");
-		replay.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		replay.setForeground(Color.BLACK);
-		replay.setBackground(Color.WHITE);
-		add(replay, BorderLayout.CENTER);
-		
-		replay.addMouseListener(new CustomMouseListener());
+		btnHome = new JButton();
+		btnHome.setOpaque(false);
+		btnHome.setFocusPainted(false);
+		btnHome.setBorderPainted(false);
+		btnHome.setContentAreaFilled(false);
+		setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
+		btnHome.setIcon(new ImageIcon("data/btnHome.png"));
+		GridBagConstraints gbc_btnBack = new GridBagConstraints();
+		gbc_btnBack.gridx = 1;
+		gbc_btnBack.gridy = 1;
+		add(btnHome, gbc_btnBack);
+		btnHome.addMouseListener(new MouseHome());
 	}
 	
 	
-	class CustomMouseListener implements MouseListener {
+	
+	class MouseHome implements MouseListener {
 
 		@Override
 		public void mouseClicked(MouseEvent e) {
+			try {
+				container.getSinglePlayerScene().getPlayScene().setUp();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			container.showMenuScene();
 		}
 
@@ -68,12 +97,12 @@ public class WinScene extends JPanel {
 
 		@Override
 		public void mouseEntered(MouseEvent e) {
-			
+			btnHome.setIcon(new ImageIcon("data/btnHomePress.png"));
 		}
 
 		@Override
 		public void mouseExited(MouseEvent e) {
-			
+			btnHome.setIcon(new ImageIcon("data/btnHome.png"));
 		}
 		
 	}
